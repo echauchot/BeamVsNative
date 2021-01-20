e93ec8c65101e248eb4a6b7068d6c9f79186f729
Etienne Chauchot <echauchot@apache.org>
2021-01-20 14:32:19 +0100

    Fixups for first run

--- beam-pipelines/src/main/java/org/example/beam/Benchmark.java
+++ beam-pipelines/src/main/java/org/example/beam/Benchmark.java
@@ -8,6 +8,7 @@ import org.apache.beam.sdk.options.PipelineOptions;
 import org.apache.beam.sdk.options.PipelineOptionsFactory;
 import org.apache.beam.sdk.transforms.PTransform;
 import org.apache.beam.sdk.values.PCollection;
+import org.apache.beam.vendor.grpc.v1p26p0.com.google.common.base.Strings;
 import org.example.beam.pipelines.IdentityPardo;
 import org.example.beam.pipelines.SimpleCombinePerKey;
 import org.example.beam.pipelines.SimpleGroupByKey;
@@ -17,7 +18,7 @@ import org.slf4j.LoggerFactory;
 
 public class Benchmark {
 
-  private static final String INPUT_FILE = "../input/GDELT.MASTERREDUCEDV2.TXT";
+  private static final String INPUT_FILE = "/home/echauchot/projects/beamVsNative/input/GDELT.MASTERREDUCEDV2.TXT";
   private static final Logger LOG = LoggerFactory.getLogger(Benchmark.class);
 
   private static PTransform<PCollection<String>, ? extends PCollection<? extends Serializable>> instanciatePTransform(
@@ -36,12 +37,15 @@ public class Benchmark {
 
   public static void main(String[] args) throws Exception {
 
-    final Options pipelineOptions = PipelineOptionsFactory.fromArgs(args).withValidation()
-      .as(Options.class);
+    final Options pipelineOptions = PipelineOptionsFactory.fromArgs(args).withValidation().as(Options.class);
     final String pipelineToRun = pipelineOptions.getPipeline();
+    if (Strings.isNullOrEmpty(pipelineToRun)){
+      throw new RuntimeException("Please specify a valid pipeline among IdentityPardo, SimpleGroupByKey or SimpleCombinePerKey");
+    }
     Pipeline pipeline = Pipeline.create(pipelineOptions);
     PCollection<String> input = pipeline.apply("ReadFromGDELTFile", TextIO.read().from(INPUT_FILE));
     input.apply(pipelineToRun, instanciatePTransform(pipelineToRun));
+    LOG.info("Benchmark starting on Beam {} runner", pipelineOptions.getRunner());
     final long start = System.currentTimeMillis();
     pipeline.run();
     final long end = System.currentTimeMillis();
@@ -50,7 +54,8 @@ public class Benchmark {
 
   public interface Options extends PipelineOptions {
 
-    @Description("Pipeline to run: IdentityPardo, SimpleGroupByKey, SimpleCombinePerKey") String getPipeline();
+    @Description("Pipeline to run: IdentityPardo, SimpleGroupByKey, SimpleCombinePerKey")
+    String getPipeline();
 
     void setPipeline(String value);
   }

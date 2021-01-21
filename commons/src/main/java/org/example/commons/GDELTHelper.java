9e7dd1d7e808641cbe16add5300c8c513a20fa9a
Etienne Chauchot <echauchot@apache.org>
2021-01-21 10:41:46 +0100

    Adapt to reduced GDELT format

--- commons/src/main/java/org/example/commons/GDELTHelper.java
+++ commons/src/main/java/org/example/commons/GDELTHelper.java
@@ -3,21 +3,12 @@ package org.example.commons;
 public class GDELTHelper {
   public static String getCountry(String row) {
     String[] fields = row.split("\\t+");
-    if (fields.length > 22) {
-      if (fields[21].length() > 2) {
-        return fields[21].substring(0, 1);
-      }
-      return fields[21];
-    }
-    return "NA";
+    return fields[1];
   }
 
   public static String getSubject(String row) {
     String[] fields = row.split("\\t+");
-    if (fields.length >= 7 && fields[6].length() > 0) {
-      return fields[6];
-    }
-    return "NA";
+    return fields[3];
   }
 
 }

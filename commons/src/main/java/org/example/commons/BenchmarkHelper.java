d780067e95b468f0ffa8afcebcb2578e34bc7308
Etienne Chauchot <echauchot@apache.org>
2021-01-22 11:17:32 +0100

    Fix log results to output file and add a test

--- commons/src/main/java/org/example/commons/BenchmarkHelper.java
+++ commons/src/main/java/org/example/commons/BenchmarkHelper.java
@@ -1,11 +1,11 @@
 package org.example.commons;
 
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
-import java.util.Calendar;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class BenchmarkHelper {
 
@@ -28,16 +28,16 @@ public class BenchmarkHelper {
   public static void logResultsToFile(String benchType, String benchEngine, String benchPipeline, String inputFile, long runTime, String outputDir)
     throws IOException {
     String header = "benchType;benchEngine;benchPipeline;inputFile;runTime";
-    String result = String.format("%s;%s;%s;%s;%s;%s", benchType, benchEngine, benchPipeline, inputFile, runTime);
+    String result = String.format("%s;%s;%s;%s;%s", benchType, benchEngine, benchPipeline, inputFile, runTime);
 
-    Calendar calendar = Calendar.getInstance();
+    SimpleDateFormat sdfDate = new SimpleDateFormat("yyyyMMdd-HH:mm:ss");
+    Date now = new Date();
     String outputFilePath = String
-      .format("%s/%s.%s.%s.%s%s%s%s%s%s", outputDir, benchType, benchEngine, benchPipeline,
-        calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH),
-        calendar.get(Calendar.DAY_OF_MONTH), calendar.get(Calendar.HOUR),
-        calendar.get(Calendar.MINUTE), calendar.get(Calendar.SECOND));
+      .format("%s/%s.%s.%s.%s", outputDir, benchType, benchEngine, benchPipeline, sdfDate.format(now));
+    new File(outputDir).mkdir();
     FileOutputStream fos = new FileOutputStream(outputFilePath);
     fos.write(header.getBytes(StandardCharsets.UTF_8));
+    fos.write('\n');
     fos.write(result.getBytes(StandardCharsets.UTF_8));
     fos.close();
   }
--- commons/src/test/java/org/example/commons/BenchmarkHelperTest.java
+++ commons/src/test/java/org/example/commons/BenchmarkHelperTest.java
@@ -1,16 +1,26 @@
 package org.example.commons;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
 
 public class BenchmarkHelperTest {
 
- private static String INPUT_LINES = "19790101\tAFR\tFRA\t043\t1\t4\t1\t2.8\t\t\t\n"
-   + "19790101\tAFR\tFRA\t050\t2\t9\t1\t3.5\t\t\t\t\t\t\t\t\t\n"
-   + "19790101\tAFR\tFRAGOV\t043\t2\t19\t1\t2.8\t1\t46\t2\t1\t46\t2\t1\t46\t2\n";
+  private static String INPUT_LINES = "19790101\tAFR\tFRA\t043\t1\t4\t1\t2.8\t\t\t\n"
+    + "19790101\tAFR\tFRA\t050\t2\t9\t1\t3.5\t\t\t\t\t\t\t\t\t\n"
+    + "19790101\tAFR\tFRAGOV\t043\t2\t19\t1\t2.8\t1\t46\t2\t1\t46\t2\t1\t46\t2\n";
+
+  @Rule
+  public TemporaryFolder temporaryFolder = new TemporaryFolder();
+
   @Test
-  public void testExtractCountrySubjectKVPairs(){
+  public void testExtractCountrySubjectKVPairs() {
     assertEquals("AFR", BenchmarkHelper.getCountry(INPUT_LINES.split("\n")[0]));
     assertEquals("043", BenchmarkHelper.getSubject(INPUT_LINES.split("\n")[0]));
     assertEquals("AFR", BenchmarkHelper.getCountry(INPUT_LINES.split("\n")[1]));
@@ -18,5 +28,20 @@ public class BenchmarkHelperTest {
     assertEquals("AFR", BenchmarkHelper.getCountry(INPUT_LINES.split("\n")[2]));
     assertEquals("043", BenchmarkHelper.getSubject(INPUT_LINES.split("\n")[2]));
 
+  }
+
+  @Test
+  public void testLogResultsToFile() throws IOException {
+    final String outputPath = temporaryFolder.getRoot().getPath();
+    final String benchType = "beam";
+    final String benchEngine = "spark";
+    final String benchPipeline = "IdentityMap";
+    BenchmarkHelper
+      .logResultsToFile(benchType, benchEngine, benchPipeline, "inputFile", 10, outputPath);
+    final String expectedFileNameFormat = String
+      .format("%s.%s.%s", benchType, benchEngine, benchPipeline);
+    final File outputDir = temporaryFolder.getRoot();
+    final String[] outputFiles = outputDir.list((dir, name) -> name.contains(expectedFileNameFormat));
+    assertTrue(outputFiles.length > 0);
   }
 }

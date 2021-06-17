a4284d87bcc5890a6a494da5766e781fbe5b8196
Etienne Chauchot <echauchot@apache.org>
2021-06-17 15:52:29 +0200

    use field positions in GBK instead of keySelector

--- flink-pipelines/src/main/java/org/example/flink/pipelines/SimpleGroupByKey.java
+++ flink-pipelines/src/main/java/org/example/flink/pipelines/SimpleGroupByKey.java
@@ -24,11 +24,7 @@ public class SimpleGroupByKey implements Operator<String> {
         }
       });
     final UnsortedGrouping<Tuple2<String, String>> unsortedGrouping = kvDataSet
-      .groupBy(new KeySelector<Tuple2<String, String>, String>() {
-        @Override public String getKey(Tuple2<String, String> element) throws Exception {
-          return element.f0;
-        }
-      });
+      .groupBy(0);
     // in flink groupBy does not provide a DataSet a transformation needs to be applied to it to get the DataSet, so apply the simplest and more efficent one
     final GroupReduceOperator<Tuple2<String, String>, Tuple2<String, String>> resultDataSet = unsortedGrouping
       .reduceGroup(new GroupReduceFunction<Tuple2<String, String>, Tuple2<String, String>>() {
